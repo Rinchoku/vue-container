b93d9df2d530eb675c96f2c31a1770ecaf66ea13
rinchoku <gazar.24865@gmail.com>
2023-02-11 21:04:34 +0900

    refactor: eslint fix

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-const http = require('http');
+const http = require("http");
 
-const hostname = '0.0.0.0';
+const hostname = "0.0.0.0";
 const port = 80;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
 });
 
 server.listen(port, hostname, () => {
