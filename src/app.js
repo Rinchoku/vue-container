8deb6c5849c4a368c1c5041eb6d834ac332f0b10
rinchoku <gazar.24865@gmail.com>
2023-02-12 16:58:10 +0900

    add: node server request html

--- src/app.js
+++ src/app.js
@@ -1,12 +1,30 @@
 const http = require("http");
+const fs = require('fs');
 
 const hostname = "0.0.0.0";
 const port = 80;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  const path = "./html" + req.url;
+
+  fs.readFile(path, function(error, content) {
+      if (error) {
+          if(error.code == 'ENOENT') {
+              fs.readFile('./html/errors/404.html', function(error, content) {
+                  res.writeHead(404, { 'Content-Type': 'text/html' });
+                  res.end(content, 'utf-8');
+              });
+          }
+          else {
+              res.writeHead(500);
+              res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+          }
+      }
+      else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf-8');
+      }
+  });
 });
 
 server.listen(port, hostname, () => {
